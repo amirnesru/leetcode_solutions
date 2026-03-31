bb0d2ad419012462e46cb1ef021747785dab73ce
amiines <amirnesru054@gmail.com>
2026-03-31 11:45:36 +0300

    Time: 7 ms (57.48%), Space: 21.2 MB (36.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,10 @@
-
 from collections import Counter
+from typing import List
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count=Counter(nums)
-        a=len(nums)/2
-        arr=[]
-        for i in count :
-            if count[i]>a:
-                arr.append(i)
-        return arr[0]
+        count = Counter(nums)
+        n = len(nums)
+        for num, freq in count.items():
+            if freq > n // 2:
+                return num

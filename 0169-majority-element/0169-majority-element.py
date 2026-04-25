9bc2bfc511dddc979b2dbb0ece89d7dbf7711181
amiines <amirnesru054@gmail.com>
2026-04-25 09:43:30 +0300

    Refactor majorityElement method for clarity

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,8 @@
 from collections import Counter
-from typing import List
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        n = len(nums)
-        for num, freq in count.items():
-            if freq > n // 2:
-                return num
+        count=Counter(nums)
+        a=len(nums)//2
+        for i in count :
+            if count[i]>a:
+                return i

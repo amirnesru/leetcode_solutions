1f7989d2e26094389206065d6366305c0f073ba4
Amir Nesru <amirnesru054@gmail.com>
2026-06-17 15:04:26 +0300

    Time: 136 ms (39.87%), Space: 56.9 MB (35.75%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -3,13 +3,13 @@ class Solution:
         m = len(boxGrid)
         n = len(boxGrid[0])
         for i in range(m):
-            ind = n - 1
+            right = n - 1
             for j in range(n - 1, -1, -1):
                 if boxGrid[i][j] == "*":
-                    ind = j - 1
+                    right = j - 1
                 elif boxGrid[i][j] == "#":
-                    boxGrid[i][j], boxGrid[i][ind] = boxGrid[i][ind], boxGrid[i][j]
-                    ind -= 1
+                    boxGrid[i][j], boxGrid[i][right] = boxGrid[i][right], boxGrid[i][j]
+                    right -= 1
         res = [[0] * m for _ in range(n)]
         for i in range(m):
             for j in range(n):
